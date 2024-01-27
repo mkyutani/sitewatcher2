03a283d5a48261655b67da2faee549a263e6425f
Miki Yutani <mkyutani@gmail.com>
2024-01-28 02:32:09 +0900

    Invalid test cases for create and get directory

--- api/test/ditrecotries.test.ts
+++ api/test/ditrecotries.test.ts
@@ -5,6 +5,30 @@ const urlBase = `http://localhost:${port}/api/v1`;
 
 const directories: string[] = [];
 
+Deno.test("415: Create a directory with a plain text", async () => {
+  const res = await fetch(`${urlBase}/directories`, {
+    method: "POST",
+    body: "plain text",
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
+  const text = await res.text();
+  assertEquals(res.status, 415);
+});
+
+Deno.test("400: Create a directory with no content", async () => {
+  const res = await fetch(`${urlBase}/directories`, {
+    method: "POST",
+    body: null,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const text = await res.text();
+  assertEquals(res.status, 400);
+});
+
 Deno.test("200: Create a directory", async () => {
   const res = await fetch(`${urlBase}/directories`, {
     method: "POST",
@@ -19,6 +43,33 @@ Deno.test("200: Create a directory", async () => {
   const text = await res.text();
   assertEquals(res.status, 200);
   directories.push(JSON.parse(text).id);
+});
+
+Deno.test("400: Create a duplicated directory", async () => {
+  const res = await fetch(`${urlBase}/directories`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "内閣官房",
+      enabled: true,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const text = await res.text();
+  assertEquals(res.status, 400);
+});
+
+Deno.test("200: Get all directories", async () => {
+  const res = await fetch(`${urlBase}/directories`);
+  const text = await res.text();
+  assertEquals(res.status, 200);
+  const json = JSON.parse(text);
+  if (json.length === 0) {
+    fail();
+  } else {
+    assertEquals(json[0].id, directories[0]);
+  }
 });
 
 Deno.test("200: Get a directory", async () => {
